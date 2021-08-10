dcc4e3d0b5f4f38d93fd81d873a7d87c4880d595
Eduardo Reyes <joeduardoreyes@outlook.com>
2021-08-09 19:30:17 -0600

    Escribiendo HTML desde JavaScript

--- descuentos/descuentos.js
+++ descuentos/descuentos.js
@@ -1,10 +1,20 @@
-const precioOriginal = 120;
-const descuento = 18;
-
 function calcularPrecioConDescuento(precio, descuento) {
   const porcentajeDelPrecioConDescuento = 100 - descuento;
   const precioConDescuento = (precio * porcentajeDelPrecioConDescuento) / 100;
   return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount() {
+  const inputPrice = document.getElementById("inputPrice"),
+    priceValue = inputPrice.value;
+  const inputDiscount = document.getElementById("inputDiscount"),
+    discountValue = inputDiscount.value;
+  const precioConDescuento = calcularPrecioConDescuento(
+    priceValue,
+    discountValue
+  );
+  const resultP = document.getElementById("resultPrice");
+  resultP.innerText = `El presio con descuento son: $${precioConDescuento} dolares`;
 }
 
 // console.log({
