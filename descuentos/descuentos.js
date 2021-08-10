a7e64837b183c76e4dcb4b15d51778a2b1a46c85
Eduardo Reyes <joeduardoreyes@outlook.com>
2021-08-10 10:16:56 -0600

    funciones inteligentes

--- descuentos/descuentos.js
+++ descuentos/descuentos.js
@@ -1,3 +1,9 @@
+const coupons = [
+  "JuanDC_es_Batman",
+  "pero_no_le_digas_a_nadie",
+  "es_un_secreto",
+];
+
 function calcularPrecioConDescuento(precio, descuento) {
   const porcentajeDelPrecioConDescuento = 100 - descuento;
   const precioConDescuento = (precio * porcentajeDelPrecioConDescuento) / 100;
@@ -7,19 +13,25 @@ function calcularPrecioConDescuento(precio, descuento) {
 function onClickButtonPriceDiscount() {
   const inputPrice = document.getElementById("inputPrice"),
     priceValue = inputPrice.value;
-  const inputDiscount = document.getElementById("inputDiscount"),
-    discountValue = inputDiscount.value;
-  const precioConDescuento = calcularPrecioConDescuento(
-    priceValue,
-    discountValue
-  );
+  const inputCoupon = document.getElementById("InputCoupon"),
+    couponValue = inputCoupon.value;
+
+  let descuento;
+
+  switch (couponValue) {
+    case "JuanDC_es_Batman":
+      descuento = 15;
+      break;
+    case "pero_no_le_digas_a_nadie":
+      descuento = 30;
+      break;
+    case "es_un_secreto":
+      descuento = 25;
+      break;
+  }
+
+  const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
   const resultP = document.getElementById("resultPrice");
   resultP.innerText = `El presio con descuento son: $${precioConDescuento} dolares`;
 }
-
-// console.log({
-//   precioOriginal,
-//   descuento,
-//   porcentajeDelPrecioConDescuento,
-//   precioConDescuento,
-// });
